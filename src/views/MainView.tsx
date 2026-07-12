18fcd8f41386c78c5535b0e2d7b0f48985baae4b
Даниил Аристов <danilka-aristov@mail.ru>
2026-07-12 17:55:44 +0300

    Переписать анимацию страниц на нативный css

--- src/views/MainView.tsx
+++ src/views/MainView.tsx
@@ -1,17 +1,9 @@
 import { useState } from 'react'
-import { useLocation, useOutlet } from 'react-router-dom'
-import { AnimatePresence, motion } from 'motion/react'
+import { Outlet, useLocation } from 'react-router-dom'
 import { BottomTabBar } from '@/components/BottomTabBar'
-import { PAGE_TRANSITION, PAGE_VARIANTS } from '@/js/constants/motion'
 
 export type MainViewOutletContext = {
   setHideTabBar: (hide: boolean) => void
-}
-
-function FrozenOutlet({ context }: { context: MainViewOutletContext }) {
-  const outlet = useOutlet(context)
-  const [frozenOutlet] = useState(outlet)
-  return frozenOutlet
 }
 
 export function MainView() {
@@ -20,19 +12,9 @@ export function MainView() {
 
   return (
     <div className={`flex flex-col flex-1 ${hideTabBar ? '' : 'pb-24'}`}>
-      <AnimatePresence mode="wait" initial={false}>
-        <motion.div
-          key={location.pathname}
-          variants={PAGE_VARIANTS}
-          initial="initial"
-          animate="animate"
-          exit="exit"
-          transition={PAGE_TRANSITION}
-          className="flex flex-col flex-1 min-h-0"
-        >
-          <FrozenOutlet context={{ setHideTabBar } satisfies MainViewOutletContext} />
-        </motion.div>
-      </AnimatePresence>
+      <div key={location.pathname} className="page-enter flex flex-col flex-1 min-h-0">
+        <Outlet context={{ setHideTabBar } satisfies MainViewOutletContext} />
+      </div>
       {!hideTabBar && <BottomTabBar />}
     </div>
   )
